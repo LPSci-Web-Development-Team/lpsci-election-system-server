03c5ca8c3282d14b847d6271e79b1be7231e47ce
pastelcolors <localcomputer1@gmail.com>
2020-01-28 11:49:25 +0800

    Add enum validation for position property in candidate model
    Available positions
    -President
    -Vice President
    -Secretary
    -Treasurer
    -Auditor
    -Public Information Officer
    -Peace Officer
    -Grade 8 Level Representative
    -Grade 9 Level Representative
    -Grade 10 Level Representative
    -Grade 11 Level Representative
    -Grade 12 Level Representative

--- common/models/candidate.js
+++ common/models/candidate.js
@@ -1,5 +1,23 @@
 'use strict';
 
-module.exports = function(Candidate) {
+const CANDIDATE_POSITIONS = [
+  'President',
+  'Vice President',
+  'Secretary',
+  'Treasurer',
+  'Auditor',
+  'Public Information Officer',
+  'Peace Officer',
+  'Grade 8 Level Representative',
+  'Grade 9 Level Representative',
+  'Grade 10 Level Representative',
+  'Grade 11 Level Representative',
+  'Grade 12 Level Representative',
+];
 
+module.exports = function (Candidate) {
+  Candidate.validatesInclusionOf('position', {
+    in: CANDIDATE_POSITIONS,
+    message: 'Invalid position for Candidate',
+  });
 };
