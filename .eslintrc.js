b17e8bc5104345c65bb221c14033b2dd55996812
Prince Neil Cedrick Castro <cedi.castro@gmail.com>
2020-06-10 05:48:53 +0800

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -38,6 +38,7 @@ module.exports = {
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-unsafe-return': 'off',
     '@typescript-eslint/no-unsafe-member-access': 'off',
+    '@typescript-eslint/no-unsafe-assignment': 'off',
     'import/extensions': [
       'error',
       'ignorePackages',
