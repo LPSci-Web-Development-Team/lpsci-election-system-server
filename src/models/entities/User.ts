aab3238e214d5d4841c122b9a029759119046431
Prince Neil Cedrick Castro <cedi.castro@gmail.com>
2020-06-10 06:20:19 +0800

    Create user entity

---
+++ src/models/entities/User.ts
@@ -0,0 +1,63 @@
+// ANCHOR Typeorm
+import {
+  Column,
+  Entity,
+  Index,
+  PrimaryColumn,
+} from 'typeorm';
+
+// ANCHOR Entities
+import { TimestampedEntity } from './common/TimestampedEntity';
+
+/* ANCHOR: User sex enum ---------------------------------------------------- */
+export enum Sex {
+  M = 'male',
+  F = 'female',
+}
+
+/* ANCHOR: User entity ------------------------------------------------------ */
+@Entity()
+export class User extends TimestampedEntity {
+  /* ANCHOR: Fields --------------------------------------------------------- */
+  @PrimaryColumn()
+  public id!: string;
+
+  @Column()
+  @Index()
+  public email!: string;
+
+  @Column()
+  public firstName!: string;
+
+  @Column()
+  public middleName!: string;
+
+  @Column()
+  public lastName!: string;
+
+  @Column()
+  public streetAddress!: string;
+
+  @Column()
+  public barangay!: string;
+
+  @Column()
+  public city!: string;
+
+  @Column({ enum: Sex })
+  public sex!: Sex;
+
+  @Column()
+  public birthDate!: Date;
+
+  @Column({ nullable: true })
+  public phoneNumber?: string;
+
+  @Column({ nullable: true })
+  public displayPhotoUuid?: string;
+
+  @Column({ default: false })
+  @Index()
+  public isAdmin!: boolean;
+
+/* ANCHOR: Relations ------------------------------------------------------ */
