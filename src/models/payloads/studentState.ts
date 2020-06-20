0bdf278f4e8c7d5282a7bc9e217881fc1697d8ce
Prince Neil Cedrick Castro <cedi.castro@gmail.com>
2020-06-20 10:46:48 +0800

    Create student state payload

--- src/models/payloads/studentState.ts
+++ src/models/payloads/studentState.ts
@@ -1,6 +1,41 @@
+// ANCHOR Entities
+import { StudentState } from '../entities/StudentState';
+
+// ANCHOR Payload
+import { IFetchUserPayload } from './user';
+
 export enum EStudentState {
   Enrolled = 'enrolled',
   Dropped = 'dropped',
   Transfered = 'transfered',
   Graduated = 'graduated',
 }
+
+export interface ICreateStudentStatePayload {
+  state: EStudentState;
+}
+
+export interface IFetchStudentStatePayload {
+  id: string;
+  state: EStudentState;
+  schoolYear: string;
+  users?: IFetchUserPayload[];
+}
+
+export const studentStateToFetchPayload = (
+  studentState: StudentState,
+) => {
+  const {
+    id,
+    state,
+    schoolYear,
+    users,
+  } = studentState;
+
+  return {
+    id,
+    state,
+    schoolYear,
+    users,
+  };
+};
