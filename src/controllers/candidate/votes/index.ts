2aedf44809ae0156fdaead021be35849e0285d70
Prince Neil Cedrick Castro <cedi.castro@gmail.com>
2020-06-21 19:18:42 +0800

    Create vote controller

---
+++ src/controllers/candidate/votes/index.ts
@@ -0,0 +1,31 @@
+// ANCHOR Typeorm
+import { getRepository } from 'typeorm';
+
+// ANCHOR Entities
+import { Candidate } from '../../../models/entities/Candidate';
+
+// ANCHOR Error
+import { NotFoundError } from '../../../errors/custom/NotFound';
+
+/**
+ * ANCHOR: Get all votes
+ *
+ * @param candidate Candidate
+ */
+export const getAllSectionsForCandidate = async (
+  { id }: Candidate,
+) => {
+  const candidate = await getRepository(Candidate)
+    .findOne({
+      where: {
+        id,
+      },
+      relations: ['votes'],
+    });
+
+  if (!candidate) {
+    throw new NotFoundError(`Candidate with id of ${id} could not be found`);
+  }
+
+  return candidate.votes;
+};
---
+++ src/controllers/student/votes/index.ts
@@ -0,0 +1,31 @@
+// ANCHOR Typeorm
+import { getRepository } from 'typeorm';
+
+// ANCHOR Entities
+import { Student } from '../../../models/entities/Student';
+
+// ANCHOR Error
+import { NotFoundError } from '../../../errors/custom/NotFound';
+
+/**
+ * ANCHOR: Get all votes
+ *
+ * @param student Student
+ */
+export const getAllSectionsForStudent = async (
+  { id }: Student,
+) => {
+  const student = await getRepository(Student)
+    .findOne({
+      where: {
+        id,
+      },
+      relations: ['votes'],
+    });
+
+  if (!student) {
+    throw new NotFoundError(`Student with id of ${id} could not be found`);
+  }
+
+  return student.votes;
+};
